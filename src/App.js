08f0b420bd3acc33a155ec4e20a8d245d0c8e05b
Saksham <sakshamdev3@gmail.com>
2023-10-03 01:25:10 +0530

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { HashRouter,Routes,Route } from "react-router-dom";
 
 import Login from "./pages/Login/Login"
 import LandingPage from "./pages/LandingPage/LandingPage";
@@ -9,15 +9,17 @@ import NoPage from "./pages/NoPage";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
             <Route index element= {<Login/>} />
+            <Route path='/react-CollabSphere-client' element ={<Login/>} />
+            <Route path='/index' element ={<Login/>} />
             <Route path='/Login' element ={<Login/>} />
             <Route path='/LandingPage' element ={<LandingPage />} />
             <Route path='/InRoom' element ={<InRoom />} />
             <Route path='*' element ={<NoPage/>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
