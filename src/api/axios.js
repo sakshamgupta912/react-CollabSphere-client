34e0a1e78cf3f722c8aa17fd7a65d35a9e5a2364
sakshamgupta912 <saksham.gupta.btech2021@sitpune.edu.in>
2024-04-17 00:39:20 +0530

    Changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://collabspheresever.azurewebsites.net',
+    baseURL: 'https://collabsphere-server.onrender.com',
 })
